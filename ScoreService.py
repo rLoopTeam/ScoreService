60fc7a00d82d9b97e3f15cedd6a88689f124c5da
foxlet <foxlet@furcode.co>
2016-03-23 01:24:21 -0400

    Add DB logic.

--- ScoreService.py
+++ ScoreService.py
@@ -1,13 +1,16 @@
 from flask import Flask, request, jsonify
 from flask import render_template
-import random
 from util import cors
+
+from pymongo import MongoClient
 
 app = Flask(__name__)
 
-ROOT_APP = "/home/furserv/chanpage-gen/"
-ROOT_DOMAIN = "http://score.furcode.co"
+client = MongoClient()
 
+ROOT_DOMAIN = "http://rloop.org/"
+
+db = client.player_db
 
 @app.route('/')
 @cors.crossdomain(origin="*")
@@ -20,18 +23,61 @@ def render_error():
 @cors.crossdomain(origin="*", headers="Content-type")
 def api_upsert_user_score():
     payload = request.json
-    #Placeholder Function
-    return jsonify(playerName=payload['playerName'], score=payload['score'], message='Upload successful.')
+    payload['playerName'] = payload['playerName'].lower()
+    payload['score'] = int(payload['score'])
+
+    query = db.player_score.update_one({'player': payload['playerName']},
+                                       {'$max': {'score': payload['score']}}, True)
+
+    return jsonify(playerName=payload['playerName'], message='Upload successful.')
 
 
 @app.route('/api/GetUserRank', methods=['POST', 'OPTIONS'])
 @cors.crossdomain(origin="*", headers="Content-type")
 def api_get_user_rank():
     payload = request.json
-    #Placeholder Function
-    payload['rank'] = random.randint(1, 50)
-    payload['score'] = random.randint(1000, 30000)
-    return jsonify(playerName=payload['playerName'], rank=payload['rank'], score=payload['score'])
+    payload['playerName'] = payload['playerName'].lower()
+
+    if db.player_score.count ({'player': payload['playerName']}) == 0:
+        return jsonify(error='Could not find such player.')
+    else:
+        cursor = db.player_score.find_one({'player': payload['playerName']})
+
+    enum = db.player_score.find().sort('score', -1)
+    rank = 1
+    for item in enum:
+        if item['player'] == payload['playerName']:
+            break
+        else:
+            rank += 1
+
+    return jsonify(playerName=cursor['player'], rank=rank, score=cursor['score'])
+
+
+@app.route('/api/GetNearUsers', methods=['POST', 'OPTIONS'])
+@cors.crossdomain(origin="*", headers="Content-type")
+def api_get_near_users():
+    payload = request.json
+    payload['playerName'] = payload['playerName'].lower()
+
+    if db.player_score.count ({'player': payload['playerName']}) == 0:
+        return jsonify(error='Could not find such player.')
+
+    enum = db.player_score.find().sort('score', -1)
+    scores = []
+    range1 = []
+    count = 0
+
+    for item in enum:
+        count += 1
+        scores.append({'playerName': item['player'], 'score': item['score']})
+        if item['player'] == payload['playerName']:
+            rank = count-1
+
+    for item in scores[rank-2 if rank-2 > 0 else 0:rank+3 if rank+3 < len(scores) else len(scores)]:
+        range1.append(item)
+
+    return jsonify(near=range1, playerName=payload['playerName'], rank=rank+1)
 
 if __name__ == "__main__":
     app.run(debug=True, port=8080)
