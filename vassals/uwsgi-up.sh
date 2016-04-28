38c07fe99ced9767e951d2a00762fcde08d4b4fe
Ari Porad <ari@ariporad.com>
2016-04-27 17:15:13 -0700

    Remove some old files

--- vassals/uwsgi-up.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-pip3.4 install virtualenv
-virtualenv venv
-. venv/bin/activate
-venv/bin/pip3.4 install flask
-venv/bin/pip3.4 install requests
-venv/bin/pip3.4 install json
-deactivate
